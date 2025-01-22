fc47b8b2fc788413d9196cefbd87168c66fedb98
ashiq33 <107813420+ashiq33@users.noreply.github.com>
2025-01-22 22:03:05 +0600

    Update List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,6 +1,6 @@
 import Chatlist from "./chatList/Chatlist"
 import "./list.css"
-import Userinfo from "./userinfo/Userinfo"
+import Userinfo from "./userInfo/Userinfo"
 
 const List = () => {
     return (
